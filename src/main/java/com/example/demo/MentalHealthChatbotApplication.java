035bc2b9a9c17fba3cc41a9f82c63ccaaa788bcd
QliGenesis <kershadw@yahoo.com>
2025-04-24 13:48:54 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/MentalHealthChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthChatbotApplication.class, args);
+	}
+
+}
